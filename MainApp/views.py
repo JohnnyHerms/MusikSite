659508f7a841d1fee00b5465180b6edbe7758728
JohnnyHerms <john.hermary@gmail.com>
2016-04-04 22:07:44 -0600

    Lists all notes in each key

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import KeySignature
+
 
 def note_list(request):
-    return render(request, 'MainApp/note_list.html', {})
+    keySignatures = KeySignature.objects.prefetch_related('notes').all().order_by('rootNote__name')
+    return render(request, 'MainApp/note_list.html', {'keySignatures': keySignatures})
